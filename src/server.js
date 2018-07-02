0d604bf651091238aa8ad574a45ac9ca89dc2b21
akashtomar <akashtomar1995@yahoo.com>
2018-07-02 23:55:00 +0530

    removed unused var

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const path = require('path');
 const routes = require('../routes');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));

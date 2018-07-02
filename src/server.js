b52145d689acc927551e466499ea8365959700eb
akashtomar <akashtomar1995@yahoo.com>
2018-07-03 00:24:52 +0530

    session store

--- src/server.js
+++ src/server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
 const routes = require('../routes');
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(session({
     secret: "SecretKey",
+    store: new MongoStore({
+        url: process.env.DB_URL,
+        ttl: 10 * 60 * 60
+    }),
     resave: false,
     saveUninitialized: true
 }));
